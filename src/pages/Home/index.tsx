8cbf630be618c8609659e405a6dc372694abf28d
Nelson Plíni <nelsonplinio94@gmail.com>
2020-07-28 12:15:04 -0300

    Feat: Add list of tools and design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,31 +1,121 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { BsPlus } from 'react-icons/bs';
+
+import api from '../../services/api';
+
+import iconClose from '../../assets/icons/Icon-Close-2px.svg';
 
 import {
   Container,
   Header,
   Toolbar,
   SearchBar,
-  AddNewToolButton,
+  Button,
+  List,
+  ToolCard,
+  TagsContainer,
 } from './styles';
 
+interface Tool {
+  id: string;
+  title: string;
+  description: string;
+  tags: string[];
+  link: string;
+}
+
 const Home: React.FC = () => {
+  const [tools, setTools] = useState<Tool[]>([]);
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    const loadTools = async () => {
+      let tag: string | undefined;
+
+      if (searchText.includes('#')) {
+        const [, tagFound] = searchText.split('#');
+        tag = tagFound;
+      }
+
+      const response = await api.get('/tools', {
+        params: { tag },
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTU5MDYxODgsImV4cCI6MTU5NTk5MjU4OCwic3ViIjoiNWYxYjQ1NmVlZTJhODcwYzdjOTdkYjk3In0.wwKXSwK1ZvnOhkNOzb0E3ijStCdw8O6Kwtkb6CVSf6g',
+        },
+      });
+
+      setTools(response.data);
+    };
+
+    loadTools();
+  }, [searchText]);
+
+  const handleSelectTag = useCallback(
+    (tag: string) => {
+      setSearchText(tag);
+    },
+    [setSearchText],
+  );
+
   return (
     <Container>
       <Header>
         <h1>VUTTR</h1>
+
         <h3>Very Useful Tools to Remember</h3>
+
         <Toolbar>
           <SearchBar>
-            <input type="text" placeholder="Buscar por tag" />
+            <input
+              type="text"
+              placeholder="Buscar por tag (ex: #node)"
+              value={searchText}
+            />
           </SearchBar>
 
-          <AddNewToolButton>
+          <Button type="button" primaryNeutral>
             <BsPlus />
             Adicionar
-          </AddNewToolButton>
+          </Button>
         </Toolbar>
       </Header>
+
+      <List>
+        {tools.map(tool => (
+          <ToolCard key={tool.id}>
+            <div>
+              <h4>
+                <a href={tool.link} target="__blank">
+                  {tool.title}
+                </a>
+              </h4>
+
+              <Button type="button" quartiaryDanger>
+                Remover
+              </Button>
+            </div>
+
+            <p>{tool.description}</p>
+
+            <TagsContainer>
+              {tool.tags.map(tag => (
+                <li key={tag}>
+                  <button
+                    type="button"
+                    onClick={() => handleSelectTag(`#${tag}`)}
+                  >
+                    <strong>
+                      <span>#</span>
+                      {tag}
+                    </strong>
+                  </button>
+                </li>
+              ))}
+            </TagsContainer>
+          </ToolCard>
+        ))}
+      </List>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,10 +1,16 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ButtonProps {
+  primaryNeutral?: boolean;
+  quartiaryDanger?: boolean;
+}
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 950px;
+  max-width: 982px;
   margin: 32px auto;
   height: 100vh;
+  padding: 0 16px;
 `;
 
 export const Header = styled.header``;
@@ -18,6 +24,7 @@ export const Toolbar = styled.div`
 
 export const SearchBar = styled.div`
   flex: 1;
+  margin-right: 8px;
   input {
     width: 100%;
     max-width: 360px;
@@ -38,26 +45,144 @@ export const SearchBar = styled.div`
   }
 `;
 
-export const AddNewToolButton = styled.button`
-  background: #365df0;
+export const Button = styled.button<ButtonProps>`
   color: #ffffff;
   display: flex;
   align-items: center;
   justify-content: center;
   border: none;
   padding: 8px 16px;
+  height: 50px;
   border-radius: 5px;
   transition: background 0.2s;
 
-  &:hover {
-    background: #2f55cc;
-  }
-
-  &:active {
-    background: #244aa8;
-  }
-
-  svg {
+  svg,
+  img {
+    color: #ffffff;
     margin-right: 8px;
+  }
+
+  ${({ primaryNeutral }) =>
+    primaryNeutral &&
+    css`
+      background: #365df0;
+      &:hover {
+        background: #2f55cc;
+      }
+
+      &:active {
+        background: #244aa8;
+      }
+    `}
+
+  ${({ quartiaryDanger }) =>
+    quartiaryDanger &&
+    css`
+      transition: color 0.2s;
+      color: #f95e5a;
+      background: none;
+
+      svg {
+        color: #f95e5a;
+      }
+
+      &:hover {
+        color: #cc4c4c;
+
+        svg {
+          color: #cc4c4c;
+        }
+      }
+
+      &:active {
+        color: #a53f3f;
+
+        svg {
+          color: #a53f3f;
+        }
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin: 32px 0;
+`;
+
+export const ToolCard = styled.li`
+  border-radius: 5px;
+  background: #fff;
+  border: solid 1px #ebeaed;
+  box-shadow: 0px 5px 7px #0000000d;
+  padding: 16px;
+  overflow: hidden;
+
+  & + li {
+    margin-top: 16px;
+  }
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  a {
+    text-decoration: none;
+    color: none;
+  }
+
+  > p {
+    margin: 16px 0;
+    /* font-size: 22px; */
+    line-height: 24px;
+  }
+`;
+
+export const TagsContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  > li {
+    position: relative;
+    cursor: pointer;
+
+    &:after {
+      content: '';
+      position: absolute;
+      background: #170c3a;
+      height: 2px;
+      width: 100%;
+      max-width: 0;
+      bottom: -4px;
+      left: calc(100% - 50%);
+      opacity: 0;
+      border-radius: 1px;
+      transition: max-width 0.3s, opacity 0.5s, left 0.3s;
+      margin: auto;
+    }
+
+    &:hover:after {
+      max-width: 100%;
+      opacity: 1;
+      left: 0;
+    }
+
+    span {
+      font-size: 16px;
+      opacity: 0.7;
+      margin-right: 2px;
+    }
+
+    button {
+      background: none;
+      border: none;
+    }
+  }
+
+  > li + li {
+    margin-left: 10px;
   }
 `;
